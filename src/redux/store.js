74ba3d3422985bb4cd8791fe8c13e933aa47e1b4
Konstantyn <k.buhantsev@gmail.com>
2022-11-01 16:35:51 +0200

    bjhjh

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,16 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { rootReducer } from "./reducer";
 
-const enhancer = devToolsEnhancer();
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
 
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from "@reduxjs/toolkit";
+import { tasksReducer, filtersReducer } from "./reducer";
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
